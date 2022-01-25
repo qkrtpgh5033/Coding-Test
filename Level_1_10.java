86348a987d7577d4846df3e160d1e5c7e48a0207
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-01-25 16:47:29 +0900

    Programers_Level_1-10(update)

--- Level_1_10.java
+++ Level_1_10.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.io.*;
 import java.util.*;
 
-class Level_1_10 {
+class Solution {
     public String[] solution(String[] strings, int n) {
 
-        //문자열 내 마음대로 정렬하기
-
+        // 처음 풀 떄
         ArrayList<String> list = new ArrayList<>();
 
         for(int i = 0 ; i < strings.length; i++)
@@ -25,5 +25,23 @@ class Level_1_10 {
         }
 
         return answer;
+
+        // 두 번째 풀 때
+
+//         Arrays.sort(strings, new Comparator<String>(){
+//             @Override
+//             public int compare(String s1, String s2)
+//             {
+//                 if(s1.charAt(n) > s2.charAt(n))
+//                     return 1;
+//                 else if (s1.charAt(n) == s2.charAt(n))
+//                     return s1.compareTo(s2);
+//                 else
+//                     return -1;
+//             }
+//         });
+
+//         return strings;
+
     }
 }
