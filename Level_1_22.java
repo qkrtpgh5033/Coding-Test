e5a57fa48517d579b38bd7d6b0f539f6ee3c1b23
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-02-16 09:49:13 +0900

    Programers_Level_1-22

---
+++ Level_1_22.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Level_1_22 {
+    public int solution(int n) {
+
+        // 3진법 뒤집기
+
+        ArrayList<Integer>list= new ArrayList<>();
+        int answer = 0;
+
+        while(n>0)
+        {
+            int num = n % 3;
+            list.add(num);
+            n /= 3;
+        }
+
+
+        for(int i = 0 ; i < list.size(); i++)
+        {
+            answer += list.get(list.size() - (i+1)) * Math.pow(3,i);
+        }
+        return answer;
+    }
+}
