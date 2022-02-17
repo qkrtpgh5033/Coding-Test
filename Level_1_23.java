4789200b8eace24a09b1ba83a435d8fbe66af425
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-02-17 10:38:33 +0900

    Programers_Level_1-23

---
+++ Level_1_23.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Level_1_23 {
+    public int solution(int[] nums) {
+
+        //폰켓몬 ArrayList 풀이
+        Arrays.sort(nums);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i : nums)
+        {
+            if(!list.contains(i)) // 핵심
+                list.add(i);
+        }
+
+        int selectLen = nums.length / 2;
+
+
+        if(list.size() >= selectLen)
+            return selectLen;
+        else
+            return list.size();
+
+
+    }
+}
