155a0535cdb5d54b0319d00ccf73a7686615f59c
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-02-23 11:46:28 +0900

    Programers_Level_1-26(코드 간결화)"

--- Level_1_26.java
+++ Level_1_26.java
@@ -1,51 +1,25 @@
 class Level_1_26 {
     public String[] solution(int n, int[] arr1, int[] arr2) {
 
-        // 비밀지도
+        // 비밀지도 (수정)
 
         String[] answer = new String[n];
 
         for(int i = 0 ; i < arr1.length; i++)
         {
 
-            String binary1 = Integer.toBinaryString(arr1[i]);
-            String binary2 = Integer.toBinaryString(arr2[i]);
+            String binary = Integer.toBinaryString(arr1[i] | arr2[i]);
 
-            String binary1_m = "";
-            String binary2_m = "";
-
-            if(binary1.length() < n)
+            if(binary.length() < n)
             {
-                int length = n - binary1.length();
+                int length = n - binary.length();
                 for(int j = 0 ; j < length; j++)
                 {
-                    binary1_m += "0";
+                    binary = "0" + binary;
                 }
-                binary1 = binary1_m + binary1;
             }
 
-            if(binary2.length() < n)
-            {
-                int length = n - binary2.length();
-                for(int j = 0 ; j < length; j++)
-                {
-                    binary2_m += "0";
-                }
-                binary2 = binary2_m + binary2;
-            }
-
-
-            String secret = "";
-            for(int j = 0; j  < n; j++)
-            {
-                if(binary1.charAt(j) == '1' || binary2.charAt(j) == '1')
-                    secret += "#";
-                else
-                    secret += " ";
-            }
-
-            answer[i] = secret;
-
+            answer[i] = binary.replaceAll("1","#").replaceAll("0"," ");
 
         }
         return answer;
