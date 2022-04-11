6bffc76f304459cc7a132e315ddb337aed65b284
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-04-11 10:12:47 +0900

    2. 보이는 학생(★)

---
+++ Algorithm-Test/String/Problem02_02.java
@@ -0,0 +1,37 @@
+package String;
+
+import java.util.*;
+
+public class Problem02_02 {
+
+    public void solution(int [] arr)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        int max = arr[0];
+
+        for(int i = 1 ; i < arr.length; i++)
+        {
+          if(max < arr[i])
+          {
+              max = arr[i];
+              list.add(max);
+          }
+        }
+
+        System.out.println(list.size());
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [] arr = new int [n];
+        for(int i = 0; i < arr.length; i++)
+            arr[i] = sc.nextInt();
+
+        Problem02_02 T = new Problem02_02();
+        T.solution(arr);
+
+    }
+}
