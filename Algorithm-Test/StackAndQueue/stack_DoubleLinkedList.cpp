c3f41181c378604057bcf3f7db3988d6beadc67c
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-05-26 22:27:10 +0900

    최종 완료

--- Algorithm-Test/StackAndQueue/stack_DoubleLinkedList.cpp
+++ Algorithm-Test/StackAndQueue/stack_DoubleLinkedList.cpp
@@ -10,8 +10,8 @@ void exit();
 
 struct NODE
 {
-	NODE*pre;
-	NODE*next;
+	NODE* pre;
+	NODE* next;
 
 	int number;
 };
@@ -20,20 +20,13 @@ NODE* head;
 NODE* tail;
 
 int main() {
+
 	int a;
-	/*
 
 	head = (NODE*)malloc(sizeof(struct NODE));
 	head->next = NULL;
 	head->pre = NULL;
 	tail = head;
-	*/
-
-	head = (NODE*)malloc(sizeof(struct NODE));
-	tail = (NODE*)malloc(sizeof(struct NODE));
-	head->next = tail;
-	tail->pre = head;
-
 
 	while (true) {
 		printf("1. push \n2. pop \n3. show all nodes \n-1. exit \n > ");
@@ -76,34 +69,46 @@ void pushNode() {
 
 	NODE* node = (NODE*)malloc(sizeof(struct NODE));
 	node->number = num;
-
+	node->next = NULL;
 
 	if (isEmpty()) {
-		head->next = node;
-		tail->pre = node;
 
-		node->next = tail;
+		/*tail = (NODE*)malloc(sizeof(struct NODE));
+		tail->next = NULL;
+		tail->pre = node;*/
+
+
+		head->next = node;
 		node->pre = head;
+
+		tail = node;
 
 	}
 	else {
-		NODE* end = tail->pre;
-		end->next = node;
-		node->pre = end;
+		NODE* prevNode = tail;
+		prevNode->next = node;
+		node->pre = prevNode;
 
-		node->next = tail;
-		tail->pre = node;
+		tail = node;
+
 	}
 }
 
 void showAllNodes() {
+
 	NODE* now = head->next; // 林家历厘
-	printf("showMethod\n");
-	while (now != tail) {
-		printf("number : %d\n", now->number);
-		now = now->next;
+	if (!isEmpty()) {
+
+		do {
+			printf("number : %d\n", now->number);
+			now = now->next;
+		} while (now != NULL);
 	}
-	printf("End\n");
+	else {
+		printf("There is no Node\n");
+	}
+
+
 }
 
 
@@ -111,39 +116,34 @@ void popNode() {
 
 	if (!isEmpty()) {
 
-		NODE* prevNode = tail->pre->pre;
-		NODE* deleteNode = prevNode->next;
-//		printf("popNode Number : %d\n", prevNode->number);
-		prevNode->next = tail;
-		tail->pre = prevNode;
+		NODE* prevNode = tail->pre;
+		NODE* deleteNode = tail;
 
-//		printf("deleteNode Number : %d\n", deleteNode->number);
+		printf("pop (0x%p) : %d\n", deleteNode, deleteNode->number);
+
 		free(deleteNode);
+		prevNode->next = NULL;
+		tail = prevNode;
+
 	}
 	else {
 		printf("There is no Node\n");
 	}
 }
 void exit() {
-	printf("headNumber : %d\n", head->number);
-	printf("headAddress : %p\n", head);
-	printf("tailNumber : %d\n", tail->number);
-	printf("tailAddress : %p\n", tail);
-	printf("tailPrevNumber : %d\n", tail->pre->number);
-
 
 	NODE* now = head; // 林家历厘
-	while (now != tail) {
+	while (now != NULL) {
 		printf("Node(%d) has been POP\n", now->number);
 		NODE* deleteNode = now;
 		now = now->next;
 		free(deleteNode);
 	}
 
-	printf("Node(%d) has been POP\n", tail->number);
+	/*printf("Node(%d) has been POP\n", tail->number);
 
 	printf("end");
-	free(tail);
+	free(tail);*/
 	exit(0);
 
 }
