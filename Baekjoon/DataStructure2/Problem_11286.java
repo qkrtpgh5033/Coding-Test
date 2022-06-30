acf36972571fa18559021023083bd479c0c82f4a
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-06-30 10:06:09 +0900

    백준 - Problem_11286번 Comparator를 통해 시간초과 문제 해결

--- Baekjoon/DataStructure2/Problem_11286.java
+++ Baekjoon/DataStructure2/Problem_11286.java
@@ -16,41 +16,66 @@ public class Problem_11286 {
 
     public static void main(String[] args) throws IOException {
 
+//        /**
+//         * 시간초과
+//         */
+//        PriorityQueue<Integer> q = new PriorityQueue<>();
+//        PriorityQueue<Integer> absQ = new PriorityQueue<>();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int size = Integer.parseInt(br.readLine());
+//
+//        for(int i = 0 ;i  < size; i++){
+//            int num = Integer.parseInt(br.readLine());
+//            if(num == 0 ){
+//                if(q.isEmpty())
+//                    System.out.println("0");
+//                else{
+//                    int peek = absQ.peek();
+//
+//                    if(q.contains(-1*peek)){
+//                        System.out.println(-peek);
+//                        q.remove(-peek);
+//
+//                    }
+//                    else{
+//                        System.out.println(peek);
+//                        q.remove(peek);
+//                    }
+//
+//                    absQ.poll();
+//                }
+//            }
+//            else {
+//                q.add(num);
+//                absQ.add(Math.abs(num));
+//            }
+//        }
         /**
-         * 시간초과
+         * Comparator로 해결
          */
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        PriorityQueue<Integer> absQ = new PriorityQueue<>();
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>((o1, o2) -> {
+            int absO1 = Math.abs(o1);
+            int absO2 = Math.abs(o2);
+
+            return (absO1 == absO2) ? (o1 > o2 ? 1 : -1) : absO1-absO2;
+
+        });
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int size = Integer.parseInt(br.readLine());
 
         for(int i = 0 ;i  < size; i++){
             int num = Integer.parseInt(br.readLine());
-            if(num == 0 ){
+            if(num == 0){
                 if(q.isEmpty())
                     System.out.println("0");
-                else{
-                    int peek = absQ.peek();
-
-                    if(q.contains(-1*peek)){
-                        System.out.println(-peek);
-                        q.remove(-peek);
-
-                    }
-                    else{
-                        System.out.println(peek);
-                        q.remove(peek);
-                    }
-
-                    absQ.poll();
-                }
+                else
+                    System.out.println(q.poll());
             }
             else {
                 q.add(num);
-                absQ.add(Math.abs(num));
             }
         }
-
-
     }
 }
