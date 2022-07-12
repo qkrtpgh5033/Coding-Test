56ac3ce2979d5b5d4d0f3cc55fee6da7887472f0
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-07-12 11:12:07 +0900

    백준 - Problem_11725번 트리의 부모 찾기

--- Baekjoon/Tree/Problem_11725.java
+++ Baekjoon/Tree/Problem_11725.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /**
+ * 백준 - 트리의 부모 찾기
+ *
  * 7
  * 1 6
  * 6 3
@@ -15,15 +17,21 @@ import java.util.StringTokenizer;
  * 2 4
  * 4 7
  */
-public class Main {
+public class Problem_11725 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
 
+
+        // 길이에 +1 해주는 이유 -> 루트 노드는 이미 1로 고정.
         ArrayList<Integer> list[] = new ArrayList[num + 1];
         boolean[] check = new boolean[num + 1];
+
+
+        // parents[a] = b --> a노드의 부모노드는 b
         int[] parents = new int[num + 1];
 
+        // 루트노드 1이 고정
         for(int i = 1; i <= num; i++){
             list[i] = new ArrayList<>();
         }
@@ -32,6 +40,7 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int node1 = Integer.parseInt(st.nextToken());
             int node2 = Integer.parseInt(st.nextToken());
+            // 서로 연결 (양방향 매핑)
             list[node1].add(node2);
             list[node2].add(node1);
         }
@@ -42,6 +51,11 @@ public class Main {
             }
         }
 
+
+        // 루트 노드는 1로 이미 고정이고, 연결된 부모 노드를 출력
+        // 1은 루트 노드이기 떄문에 부모 노드가 업고
+        // 2는 무조건 루트 노드가 1이기 때문에 이는 고려 X
+        // 따라서 문제에서 주어진 예제 출력대로 할려면 인덱스 2번째 부터 해야 됨.
         for(int i = 2; i <= num; i++)
             System.out.println(parents[i]);
 
@@ -50,15 +64,19 @@ public class Main {
     private static void dfs(ArrayList<Integer>[] list, int[] parents, boolean[] check, int i) {
         if(check[i])
             return;
-
+        System.out.println("--------------------------");
+        System.out.println("i = " + i);
         check[i] = true;
-        for (Integer index : list[i]) {
 
+        for (Integer index : list[i]) {
+            System.out.println("i = " + i + ",  index = " + index);
             if (!check[index]) {
+
                 parents[index] = i;
                 dfs(list, parents, check, index);
             }
         }
+
     }
 
 }
